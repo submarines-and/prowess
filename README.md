1bea947e17508d60f2e476638665bfe48af41448
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2021-04-06 07:16:19 +0200

    Localization for remaning abilities

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Prowess
-Addon that shows tooltip info if your mouseover target has a learnable pet ability. If your pet is summoned and already knows the ability or a previous rank, it will be hidden from the tooltip. I have not found a way to check if the hunter knows the ability (but has not taught the pet). If you know how to do this, please let me know.
+Addon that shows tooltip info if your mouseover target has a learnable pet ability. Data on which creature knows which ability is based on [this list](https://www.wow-petopia.com/classic/abilities.php).
+
+If your pet is summoned and already knows the ability (or a previous rank), it will be hidden from the tooltip. I have not found a way to check if the hunter knows the ability (but has not taught the pet). If you know how to do this, please let me know.
 
 
 ## Installing
@@ -7,24 +9,6 @@ Addon that shows tooltip info if your mouseover target has a learnable pet abili
 * Rename the folder `prowess`. Remember to remove any excessive folders; there should only be one folder containing all the files.
 * Move to your `interface/addons` directory.
 * Restart your game to load the addon.
-
-
-## Supported abilities
-* Bite
-* Claw
-* Dash
-* Prowl
-
-
-## Where is the rest?
-There is no way I am copy/pasting spell- and creature ids for things that I have no intention of using. If you have spare time and want to get carpel tunnel syndrome, feel free to update `database.lua` with any missing abilities. 
-
-Sorry! :)
-
-__To add a new ability__
-* Find your ability [here](https://www.wow-petopia.com/classic/abilities.php).
-* Map id of all creatures that know your spell to the spell id in `database.lua`.
-* Add display name if missing to `localization.lua`.
 
 
 ## Localization
--- localization.lua
+++ localization.lua
@@ -16,6 +16,14 @@ namespace.displayNames = {
         ["17260"] = "Bite 7",
         ["17261"] = "Bite 8",
 
+        -- Charge
+        ["7371"] = "Charge 1",
+        ["26177"] = "Charge 2",
+        ["26178"] = "Charge 3",
+        ["26179"] = "Charge 4",
+        ["26201"] = "Charge 5",
+        ["27685"] = "Charge 6",
+
         -- Claw
         ["16827"] = "Claw 1",
         ["16828"] = "Claw 2",
@@ -26,15 +34,62 @@ namespace.displayNames = {
         ["3010"] = "Claw 7",
         ["3009"] = "Claw 8",
 
+        -- Cower
+        ["1742"] = "Cower 1",
+        ["1753"] = "Cower 2",
+        ["1754"] = "Cower 3",
+        ["1755"] = "Cower 4",
+        ["1756"] = "Cower 5",
+        ["16697"] = "Cower 6",
+
         -- Dash
         ["23099"] = "Dash 1",
         ["23109"] = "Dash 2",
         ["23110"] = "Dash 3",
 
+        -- Dive
+        ["23145"] = "Dive 1",
+        ["23147"] = "Dive 2",
+        ["23148"] = "Dive 3",
+
+        -- Furious Howl
+        ["24604"] = "Furious Howl 1",
+        ["24605"] = "Furious Howl 2",
+        ["24603"] = "Furious Howl 3",
+        ["24597"] = "Furious Howl 4",
+
+        -- Lightning Breath
+        ["24844"] = "Lightning Breath 1",
+        ["25008"] = "Lightning Breath 2",
+        ["25009"] = "Lightning Breath 3",
+        ["25010"] = "Lightning Breath 4",
+        ["25011"] = "Lightning Breath 5",
+        ["25012"] = "Lightning Breath 6",
+
         -- Prowl
         ["24450"] = "Prowl 1",
         ["24452"] = "Prowl 2",
-        ["24453"] = "Prowl 3"
+        ["24453"] = "Prowl 3",
+
+        -- Scorpid Poison
+        ["24640"] = "Scorpid Poison 1",
+        ["24583"] = "Scorpid Poison 2",
+        ["24586"] = "Scorpid Poison 3",
+        ["24587"] = "Scorpid Poison 4",
+
+        -- Screech
+        ["24423"] = "Screech 1",
+        ["24577"] = "Screech 2",
+        ["24578"] = "Screech 3",
+        ["24579"] = "Screech 4",
+
+        -- Shell Shield
+        ["26064"] = "Screech 1",
+
+        -- Thunderstomp
+        ["26090"] = "Thunderstomp 1",
+        ["26187"] = "Thunderstomp 2",
+        ["26188"] = "Thunderstomp 3"
     },
 
     -- German
